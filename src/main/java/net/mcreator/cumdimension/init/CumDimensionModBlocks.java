5888f9e4236d35d32429b4560a36aae5bea514e3
Ttzinh <Aluno@DESKTOP-5ME1VH9.home>
2022-07-22 14:54:27 +0100

    Added White Block

--- src/main/java/net/mcreator/cumdimension/init/CumDimensionModBlocks.java
+++ src/main/java/net/mcreator/cumdimension/init/CumDimensionModBlocks.java
@@ -10,10 +10,12 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
 
+import net.mcreator.cumdimension.block.WhiteBlock;
 import net.mcreator.cumdimension.block.CumBlockBlock;
 import net.mcreator.cumdimension.CumDimensionMod;
 
 public class CumDimensionModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, CumDimensionMod.MODID);
 	public static final RegistryObject<Block> CUM_BLOCK = REGISTRY.register("cum_block", () -> new CumBlockBlock());
+	public static final RegistryObject<Block> WHITE = REGISTRY.register("white", () -> new WhiteBlock());
 }
